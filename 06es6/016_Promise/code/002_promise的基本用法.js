086d6fb268196213c8c91e2cff0da832ec3f759b
jcs <rosejcs@163.com>
2019-07-07 15:04:06 +0800

    addpromise0707

--- 06es6/016_Promise/code/002_promise的基本用法.js
+++ 06es6/016_Promise/code/002_promise的基本用法.js
@@ -6,3 +6,31 @@ function timeout(ms) {
 timeout(100).then(value => {
   console.log(value);
 });
+
+
+function loadImageUrl(url) {
+	return new Promise((resolve,reject)=>{
+		const image = new Image()
+		image.onload = function () {
+			resolve(image);
+		}
+		image.onerror = function () {
+			reject(new Error('could not load image at' + url))
+		}
+		image.src = url
+	})
+}
+	
+	
+
+window.onload = loadImageUrl('./1.jpg').then(result=>{
+	console.log(result)
+	let div = document.createElement('div');
+	div.appendChild(result);
+	document.body.appendChild(div)
+	console.log(div)
+})
+
+
+
+
